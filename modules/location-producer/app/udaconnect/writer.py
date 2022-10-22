3f1295afa7fa29fc1816ea8d43e7c8d8a04c229a
ahmed.basha <ahmed.basha@flixbus.com>
2022-10-22 18:16:37 +0200

    fix location-producer server port

--- modules/location-producer/app/udaconnect/writer.py
+++ modules/location-producer/app/udaconnect/writer.py
@@ -5,7 +5,7 @@ import location_pb2_grpc
 
 print("Sending sample payload using gRPC...")
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel("localhost:30003")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 location = location_pb2.LocationMessage(
